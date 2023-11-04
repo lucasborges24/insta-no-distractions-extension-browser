775eb089a5de9405925d59d3ded0eaa19f94355a
Lucas Borges Barbosa <lucasborgesbarbosa2012@gmail.com>
2023-11-04 14:04:41 -0300

    fix: enhance recommended accounts is not displayed

--- content.js
+++ content.js
@@ -1,23 +1,35 @@
-recommendedIsRemoved = false;
-function removeContent() {
+function getRecommendedElement() {
   const father =
-    document.body.childNodes[1]?.childNodes[0]?.childNodes[0]?.childNodes[1]
+    document.body?.childNodes[1]?.childNodes[0]?.childNodes[0]?.childNodes[1]
       ?.childNodes[0]?.firstChild?.firstChild?.firstChild?.firstChild;
   if (!father) return;
-  const sidebar = father.childNodes[0];
-  const main = father.childNodes[1].firstChild.firstChild.firstChild;
-  const recommended = main?.childNodes[1]?.firstChild.childNodes[1];
-  if (recommended) {
-    removeRecommendedElement(recommended);
+  const main = father?.childNodes[1]?.firstChild?.firstChild?.firstChild;
+  const recommendedElement = main?.childNodes[1]?.firstChild?.childNodes[1];
+  return recommendedElement;
+}
+
+function removeContent() {
+  const recommendedElement = getRecommendedElement();
+
+  if (recommendedElement) {
+    recommendedElement.style.display = "none";
+    recommendedElement.previousSibling.style.margin = "1.25rem 0";
   }
 }
 
-const removeRecommendedElement = (recommendedElement) => {
-  if (recommendedIsRemoved) return;
-  recommendedElement.remove();
-  recommendedIsRemoved = true;
-};
+function observeDOM() {
+  const targetNode = document.querySelector("body");
+  const config = { childList: true, subtree: true };
+
+  const callback = function (mutationsList, observer) {
+    removeContent();
+  };
+
+  const observer = new MutationObserver(callback);
+
+  observer.observe(targetNode, config);
+}
 
 window.addEventListener("load", () => {
-  removeContent();
+  observeDOM();
 });
